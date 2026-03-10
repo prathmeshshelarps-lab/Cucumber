4d2a6160eb3a6304bd8efdb7ce000ad4d2fdcc2b
Prathmesh Shelar <prathmeshshelarps@gmail.com>
2026-03-10 15:45:19 +0530

    Runner changed

--- TestRunner.java
+++ TestRunner.java
@@ -1,21 +1,15 @@
 package org.example;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
+@RunWith(Cucumber.class)
 @CucumberOptions(
-
         features = "src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        plugin = {
-                "pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-        }
-
+        plugin = {"pretty","html:target/cucumber-report.html"}
 )
 
-public class TestRunner extends AbstractTestNGCucumberTests {
-
+public class TestRunner {
 }
